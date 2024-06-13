efb1dd12c8cd13766222f922212c013e2a9a04a1
pravallika-0506 <111057227+pravallika-0506@users.noreply.github.com>
2024-06-13 15:33:03 +0530

    Create yeedu_operator.py

---
+++ code/yeedu_operator.py
@@ -0,0 +1,11 @@
+from prefect import flow, task
+from prefect.runner.storage import GitRepository
+from prefect.blocks.system import Secret
+from operators.yeedu import YeeduOperator
+
+@flow(retries=0, retry_delay_seconds=5, log_prints=True)
+def parse_job_url():
+    operator = YeeduOperator(job_url='https://qa-gcp.yeedu.io/tenant/bf34353f-0df2-4015-a3fc-85ac437f967e/workspace/2/spark/conf/4/metrics?type=spark_job&page=1&limit=20',
+                             connection_name='secureconnection',
+                             login_password='loginpassword')
+operator.execute()
